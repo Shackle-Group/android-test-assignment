38b00a363d7e42626ac77a5a4c72118baaf5cf2f
Adrian Czuczka <adrianczuczka@gmail.com>
2023-08-05 17:20:15 -0700

    Move main activity

--- app/src/main/java/com/example/shacklehotelbuddy/ui/MainActivity.kt
+++ app/src/main/java/com/example/shacklehotelbuddy/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.shacklehotelbuddy
+package com.example.shacklehotelbuddy.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -17,6 +17,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.shacklehotelbuddy.R
 import com.example.shacklehotelbuddy.ui.theme.ShackleHotelBuddyTheme
 
 class MainActivity : ComponentActivity() {
