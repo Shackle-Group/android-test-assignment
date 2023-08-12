801daadb3f22995417ecea4b5e44f97007a2f194
Adrian Czuczka <adrianczuczka@gmail.com>
2023-08-12 10:46:01 +0900

    Add icon to search screen

--- features/search/src/main/java/com/adrianczuczka/search/SearchScreen.kt
+++ features/search/src/main/java/com/adrianczuczka/search/SearchScreen.kt
@@ -1,8 +1,30 @@
 package com.adrianczuczka.search
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 
 @Composable
 fun SearchScreen() {
-
+    Column {
+        LazyColumn {
+            item {
+                Text(text = stringResource(id = R.string.search_screen_title_text))
+            }
+            item {
+                Row {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_event_upcoming),
+                        contentDescription = null
+                    )
+                    Text(text = stringResource(id = R.string.search_screen_check_in_date_text))
+                }
+            }
+        }
+    }
 }
