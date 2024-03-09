b6d168f90143d1104dc42e9c9771b6e692d7a7a9
tjabid <TajjalleeAbid@gmail.com>
2024-03-09 18:35:45 +0400

    mocks implemented

--- app/src/test/java/com/example/shacklehotelbuddy/data/local/datasource/HotelLocalDataSourceImplTest.kt
+++ app/src/test/java/com/example/shacklehotelbuddy/data/local/datasource/HotelLocalDataSourceImplTest.kt
@@ -1,17 +1,20 @@
 package com.example.shacklehotelbuddy.data.local.datasource
 
+import com.example.shacklehotelbuddy.data.mock.MockHotelSearch
 import com.example.shacklehotelbuddy.domain.local.datasource.HotelLocalDataSource
 import com.example.shacklehotelbuddy.domain.model.HotelSearch
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class HotelLocalDataSourceImplTest @Inject constructor() : HotelLocalDataSource {
+
     override suspend fun cacheHotelSearch(hotelSearch: HotelSearch) {
-        TODO("Not yet implemented")
+
     }
 
     override suspend fun getCachedHotelSearches(): Flow<List<HotelSearch>> {
-        TODO("Not yet implemented")
+        return flow { emit(MockHotelSearch.hotelSearchList) }
     }
 
 }
